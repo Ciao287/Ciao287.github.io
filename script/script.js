1cc5b1f2d7759b256b1a6784e59ff4d000f40fec
Ciao287 <Ciao287@users.noreply.github.com>
2026-04-18 19:50:39 +0200

    Test: Dynamic CSS Switching

--- script/script.js
+++ script/script.js
@@ -2,5 +2,9 @@ const formattedTime = new Date().toLocaleTimeString(undefined, {hour: '2-digit',
 document.getElementById('hour').textContent = `Today at ${formattedTime}`;
 
 if (new Date().getMonth() === 11) {
-    document.querySelector('link.style').href = '/style/style-winter.css';
+    const styleLink = document.querySelector('.style');
+    console.log('found:', styleLink);
+    if (styleLink) {
+        styleLink.href = '/style/style-winter.css';
+    }
 };
