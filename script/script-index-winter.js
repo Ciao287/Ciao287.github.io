4a1db1644155d67e83bc34c224a772abaa023d5c
Ciao287 <Ciao287@users.noreply.github.com>
2026-04-18 21:30:06 +0200

    Fixed the Christsmas text.

--- script/script-index-winter.js
+++ script/script-index-winter.js
@@ -1,4 +1,4 @@
 const text = document.querySelector('.text')
 if (new Date().getMonth() === 11) {
-    text.textContent = "Hi, I wish you and your family a Merry Christmas!<br>I'm Ciao287, an indie developer.<br>Click one of the buttons below to learn more about me.";
+    text.textContent = "Hi, I wish you and your family a Merry Christmas!\nI'm Ciao287, an indie developer.\nClick one of the buttons below to learn more about me.";
 };
