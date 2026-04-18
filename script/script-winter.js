979eedf252140f6195c60662bae4b05cadb61a58
Ciao287 <Ciao287@users.noreply.github.com>
2026-04-18 20:23:56 +0200

    Test: Dynamic CSS Switching

---
+++ script/script-winter.js
@@ -0,0 +1,9 @@
+if (new Date().getMonth() === 11) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/style/style-winter.css';
+    const style = document.querySelector('.style');
+    if (style) style.parentNode.insertBefore(link, style);
+    else document.head.appendChild(link);
+};
